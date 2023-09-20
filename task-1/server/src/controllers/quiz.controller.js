3d97afc13148a09be8f3e1c9e249be88cdec9f7e
Shreejal Dhungana <dshreejal@gmail.com>
2023-09-20 17:28:37 +0545

    added comment for sendResponse function in controller

--- task-1/server/src/controllers/quiz.controller.js
+++ task-1/server/src/controllers/quiz.controller.js
@@ -2,6 +2,19 @@ const { sendResponse, HttpStatus } = require('../utils/apiResponse');
 const data = require('../utils/seed.js');
 const QuizModel = require('../models/quiz.model');
 
+
+/**
+ * @function sendResponse = (res, statusCode, success, data, message, error)
+ * Sends an API response with the given data and HTTP status code.
+ * @param {Object} res - The response object to send the data back to the client.
+ * @param {Number} statusCode - The HTTP status code to send in the response.
+ * @param {Boolean} success - Whether the API call was successful or not.
+ * @param {Object} data - The data to send in the response.
+ * @param {String} message - An optional message to send in the response.
+ * @param {Object} error - An optional error object to send in the response.
+ */
+
+
 //funciton to seed data to the database for easier testing in development environment. 
 //TODO: remove this function in production and replace with CRUD operation.
 exports.seedData = async (req, res) => {
