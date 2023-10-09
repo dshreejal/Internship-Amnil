2762fcf15c8c186bb73e52d34b9bd0a737fceeb1
Shreejal Dhungana <dshreejal@gmail.com>
2023-10-09 18:27:17 +0545

    minor change

--- task-6/server/src/middlewares/JwtAuthentication.middleware.js
+++ task-6/server/src/middlewares/JwtAuthentication.middleware.js
@@ -2,28 +2,29 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/User.model');
 
 const JwtAuthenticationMiddleware = async (req, res, next) => {
-    const authHeader = req.headers['authorization'];
-
-    if (!authHeader) {
-        return res.status(401).send('Access denied. No token provided');
-    }
-
-    const token = authHeader.split(' ')[1];
-
-    if (!token) {
-        return res.status(401).send('Access denied. No token provided');
-    }
-
     try {
+        const authHeader = req.headers['authorization'];
+
+        if (!authHeader) {
+            return res.status(401).send('Access denied. No token provided');
+        }
+
+        const token = authHeader.split(' ')[1];
+
+        if (!token) {
+            return res.status(401).send('Access denied. No token provided');
+        }
+
+
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
         if (!decoded.user) {
-            return res.status(401).send('Access denied. No token provided');
+            return res.status(401).send('Access denied. Invalid token');
         }
         const validateUser = await User.findById(decoded.user.id);
 
         if (!validateUser) {
-            return res.status(401).send('Access denied.');
+            return res.status(401).send('Access denied. User not found');
         }
 
         req.user = decoded.user;
